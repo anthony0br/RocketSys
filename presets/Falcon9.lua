f2d5f731966af85a39909eb9d56d99eb6cc8b5ac
Anthony <obrienanthony13@gmail.com>
2019-04-13 23:14:45 +0100

    Update Falcon9.lua

--- presets/Falcon9.lua
+++ presets/Falcon9.lua
@@ -1,1 +1,30 @@
-
+stage1 = {
+    name = 'stage1';
+    model = nil;
+    specificImpulseASL = x;
+    specificImpulseVac = x;
+    wetMass = x;
+    dryMass = x;
+    burnRate = x;
+    dragCoefficient = Vector3.new(0.8, 0.8, 0.8)
+}
+stage2 = {
+    name = 'stage2';
+    model = nil;
+    specificImpulseASL = x;
+    specificImpulseVac = x;
+    wetMass = x;
+    dryMass = x;
+    burnRate = x;
+    dragCoefficient = Vector3.new(0.8, 0.8, 0.8)
+}
+fairing = {
+    name = 'fairing';
+    model = nil;
+    specificImpulseASL = 0;
+    specificImpulseVac = 0;
+    wetMass = x;
+    dryMass = x;
+    burnRate = x;
+    dragCoefficient = Vector3.new(0.8, 0.25, 0.8)
+}
