f269ad407cb46ed10cc42a4e60595f95bda944cb
Anthony <obrienanthony13@gmail.com>
2019-04-13 23:46:00 +0100

    Update Classic.lua

--- examples/Classic.lua
+++ examples/Classic.lua
@@ -1,43 +1,46 @@
--- Classic style launch
--- By Sublivion
-
 RunService = game:GetService('RunService')
 rs = require(script.Parent['Rocket System III'].MainModule)
 
 stage1 = {
-	name = 'stage1';
-	model = workspace.RocketStages.Stage1;
-	specificImpulseASL = 275;
-	specificImpulseVac = 304;
-	wetMass = 150000;
-	dryMass = 15000;
-	burnRate = 750;
-	dragCoefficient = Vector3.new(0.6, 0.7, 0.6)
+    name = 'stage1';
+    model = workspace.RocketStages.Stage1;
+    specificImpulseASL =282;
+    specificImpulseVac =311;
+    wetMass = 410365.5;
+    dryMass = 93490.5;
+    burnRate = 1956;
+	dragCoefficient = Vector3.new(0.8, 0.8, 0.8)
 }
 stage2 = {
-	name = 'stage2';
-	model = workspace.RocketStages.Stage2;
-	specificImpulseASL = 342;
-	specificImpulseVac = 342;
-	wetMass = 75000;
-	dryMass = 10000;
-	burnRate = 500;
-	dragCoefficient = Vector3.new(0.6, 0.7, 0.6)
+    name = 'stage2';
+    model = workspace.RocketStages.Stage2;
+    specificImpulseASL = 348;
+    specificImpulseVac = 348;
+    wetMass = 137263.5;
+    dryMass = 31638.5;
+    burnRate = 266.1;
+    dragCoefficient = Vector3.new(0.8, 0.8, 0.8)
 }
-stage3 = {
-	name = 'stage3';
-	model = workspace.RocketStages.Stage3;
-	specificImpulseASL = 0;
-	specificImpulseVac = 0;
-	wetMass = 2000;
-	dryMass = 2000;
-	burnRate = 0;
-	dragCoefficient = Vector3.new(0.6, 0.1, 0.6)
+fairing = {
+    name = 'fairing';
+    model = workspace.RocketStages.Fairing;
+    specificImpulseASL = 0;
+    specificImpulseVac = 0;
+    wetMass = 1900;
+    dryMass = 1900;
+    burnRate = 0;
+    dragCoefficient = Vector3.new(0.8, 0.25, 0.8)
 }
 
-rocket = rs.Rocket.new(stage1, stage2, stage3)
-rocket.stages.stage1:setThrottle(1)
-
+local rocket, rocket2
 RunService.Heartbeat:Connect(function()
-	rocket:update()
+	if rocket then
+		rocket:update()
+	end
+	if rocket2 then
+		rocket2:update()
+	end
 end)
+
+rocket = rs.Rocket.new(stage1, stage2, fairing)
+rocket.stages.stage1:setThrottle(1)
