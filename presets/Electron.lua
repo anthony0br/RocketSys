c19bd3f67616350fa648f8a2ba2769970312e247
AlexGF-GHub <49592843+AlexGF-GHub@users.noreply.github.com>
2019-04-14 14:11:29 +0100

    Electron.lua

--- presets/Electron.lua
+++ presets/Electron.lua
@@ -1,11 +1,11 @@
 stage1 = {
     name = 'stage1';
     model = nil;
-    specificImpulseASL = x;
-    specificImpulseVac = x;
-    wetMass = x;
-    dryMass = x;
-    burnRate = x;
+    specificImpulseASL = 303;
+    specificImpulseVac = 303;
+    wetMass = 10200;
+    dryMass = 950;
+    burnRate = 60.9;
     dragCoefficient = Vector3.new(0.8, 0.8, 0.8)
 }
 stage2 = {
@@ -13,9 +13,9 @@ stage2 = {
     model = nil;
     specificImpulseASL = x;
     specificImpulseVac = x;
-    wetMass = x;
-    dryMass = x;
-    burnRate = x;
+    wetMass = 2300;
+    dryMass = 250;
+    burnRate = 6.1;
     dragCoefficient = Vector3.new(0.8, 0.8, 0.8)
 }
 fairing = {
@@ -23,8 +23,8 @@ fairing = {
     model = nil;
     specificImpulseASL = 0;
     specificImpulseVac = 0;
-    wetMass = x;
-    dryMass = x;
-    burnRate = x;
+    wetMass = 50;
+    dryMass = 50;
+    burnRate = 0;
     dragCoefficient = Vector3.new(0.8, 0.25, 0.8)
 }
