a7566a72e3ff9d79572c89c2da59cf9aa597cc82
AlexGF-GHub <49592843+AlexGF-GHub@users.noreply.github.com>
2019-04-14 13:35:12 +0100

    FalconHeavy.lua
    The main file needs to be configured to limit Central core to TL to 70% at t+15 secs. Optional burn rate labeled wBoosterburnRate, burn rate nominal after booster sep. Cheers, Alex.

--- presets/FalconHeavy.lua
+++ presets/FalconHeavy.lua
@@ -1,30 +1,51 @@
+booster1 = {
+    name = 'stage1';
+    model = nil;
+    specificImpulseASL =282;
+    specificImpulseVac =311;
+    wetMass =410365.5;
+    dryMass = 93490.5;  
+    burnRate = 1956;
+dragCoefficient = Vector3.new(0.8,0.25, 0.8)
+}
+booster2 = {
+    name = 'stage1';
+    model = nil;
+    specificImpulseASL =282;
+    specificImpulseVac =311;
+    wetMass =410365.5;
+    dryMass = 93490.5;  
+    burnRate = 1956;
+    wBoosterburnRate = 1369.2;
+    dragCoefficient = Vector3.new(0.8,0.25, 0.8)
+}
 stage1 = {
     name = 'stage1';
     model = nil;
-    specificImpulseASL = x;
-    specificImpulseVac = x;
-    wetMass = x;
-    dryMass = x;
-    burnRate = x;
-    dragCoefficient = Vector3.new(0.8, 0.8, 0.8)
+    specificImpulseASL =282;
+    specificImpulseVac =311;
+    wetMass =410365.5;
+    dryMass = 93490.5;  
+    burnRate = 1956;
+dragCoefficient = Vector3.new(0.8,0.8, 0.8)
 }
 stage2 = {
     name = 'stage2';
     model = nil;
-    specificImpulseASL = x;
-    specificImpulseVac = x;
-    wetMass = x;
-    dryMass = x;
-    burnRate = x;
+    specificImpulseASL = 348;
+    specificImpulseVac = 348;
+    wetMass = 137263.5;
+    dryMass = 31638.5;
+    burnRate = 266.1;
     dragCoefficient = Vector3.new(0.8, 0.8, 0.8)
 }
-fairing = {
+fairing= {
     name = 'fairing';
     model = nil;
     specificImpulseASL = 0;
     specificImpulseVac = 0;
-    wetMass = x;
-    dryMass = x;
-    burnRate = x;
+    wetMass = 1900;
+    dryMass = 1900;
+    burnRate = 0;
     dragCoefficient = Vector3.new(0.8, 0.25, 0.8)
 }
